908296613840cff32b848c1872d87d3dd5cd4e3a
allexandrahu <hu.ale@northeastern.edu>
2024-04-24 13:36:50 -0700

    a6 latest

--- App.js
+++ App.js
@@ -1,40 +1,65 @@
-import express from 'express';
-import Hello from './Hello.js';
+import express from "express";
+import Hello from "./Hello.js";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
 import Lab5 from "./Lab5.js";
-import cors from 'cors';
+import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import { config } from 'dotenv';
-import session from 'express-session';
-config({ path: '.env.local' });
-
-const CONNECTION_STRING = process.env.KANBAS_DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
-
+import { config } from "dotenv";
+import session from "express-session";
+import "dotenv/config";
+config({ path: ".env.local" });
 const app = express();
-app.use(express.json());
-app.use(cors({
+Hello(app);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+//   })
+// );
+
+// CORS options
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000'
-}));
+    allowedHeaders: 'Content-Type,Authorization'
+  };
+  
+  // Enable pre-flight across-the-board
+  app.options('*', cors(corsOptions));  // Enables pre-flight request for ALL routes
+  app.use(cors(corsOptions));
+  
+//   // Your routes
+//   app.get('/api/courses', (req, res) => {
+//     res.json({ message: "Courses fetched successfully." });
+//   });
+  
+
+
+const CONNECTION_STRING =
+  process.env.KANBAS_DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+app.use(express.json());
 
 const sessionOptions = {
-    //secret: process.env.SESSION_SECRET,
-    secret: "my secret key",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.HTTP_SERVER_DOMAIN,
-    };
-}
+
 app.use(session(sessionOptions));
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+
 UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
