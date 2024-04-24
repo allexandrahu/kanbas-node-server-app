68440fc8182cbaab779ac889e9ce88b29d1b0c3e
allexandrahu <hu.ale@northeastern.edu>
2024-04-24 15:22:56 -0700

    fix

--- App.js
+++ App.js
@@ -12,53 +12,31 @@ import "dotenv/config";
 config({ path: ".env.local" });
 const app = express();
 Hello(app);
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: "http://localhost:3000",
-//   })
-// );
-
-// CORS options
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type,Authorization'
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
   };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
   
-  // Enable pre-flight across-the-board
-  app.options('*', cors(corsOptions));  // Enables pre-flight request for ALL routes
-  app.use(cors(corsOptions));
-  
-//   // Your routes
-//   app.get('/api/courses', (req, res) => {
-//     res.json({ message: "Courses fetched successfully." });
-//   });
-  
-
-
 const CONNECTION_STRING =
   process.env.KANBAS_DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
 app.use(express.json());
-
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-};
-
-app.use(session(sessionOptions));
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.HTTP_SERVER_DOMAIN,
-  };
-}
 
 UserRoutes(app);
 ModuleRoutes(app);
