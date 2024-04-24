1d9cf6a737775826f551626e67a0d8199ae9d3ec
allexandrahu <hu.ale@northeastern.edu>
2024-04-24 16:06:29 -0700

    fixed signin

--- Users/dao.js
+++ Users/dao.js
@@ -1,10 +1,15 @@
 import model from "./model.js";
-export const updateUser = (userId, user) =>
-  model.updateOne({ _id: userId }, { $set: user });
-export const findAllUsers = () => model.find();
-export const createUser = (user) => model.create(user);
+export const createUser = (user) => {
+  delete user._id
+  model.create(user);
+}
 
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
-export const findUserById = (userId) => model.findById({id: userId});
+
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) =>
+  model.findById(userId);
 export const findUsersByRole = (role) => model.find({ role: role });
-export const findUserByUsername = (username) => model.findOne({ username: username });
+export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
